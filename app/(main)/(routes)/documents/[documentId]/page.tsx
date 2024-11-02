23ca0de0892967c0de33263c181875983ed315fc
Vanshika <vanshikas2018@gmail.com>
2024-11-02 23:53:57 +0530

    editor implemented but not working

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,3 @@
-
-"use client"
 "use client";
 
 import { useMutation, useQuery } from "convex/react";
@@ -8,63 +6,66 @@ import { useMemo } from "react";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-
+import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Toolbar } from "@/components/toolbar";
-
+import { Editor } from "@/components/editor";
 
 interface DocumentIdPageProps {
-  params: {
-      documentId: Id<"documents">;
-  };
+    params: {
+        documentId: Id<"documents">;
+    };
 };
 
+const DocumentIdPage = ({
+    params
+}: DocumentIdPageProps) => {
 
-
-const DocumentIdPage = ({params}:DocumentIdPageProps) => {
-
-  const document = useQuery(api.documents.getById, {
-    documentId: params.documentId
-});
-
-const update = useMutation(api.documents.update);
-
-const onChange = (content: string) => {
-    update({
-        id: params.documentId,
-        content
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId
     });
-};
 
-if(document === undefined){
-  return <div>
-<Cover.Skeleton/>
-<div className="md:max-2-3xl lg:max-w-4xll mx-auto">
-<div className="space-y-4 pl-8 pt-4">
-<Skeleton className="h-14 w-[50p%]"/>
-<Skeleton className="h-4 w-[80p%]"/>
-<Skeleton className="h-4 w-[40p%]"/>
-<Skeleton className="h-4 w-[60p%]"/>
-</div>
-</div>
-  </div>
+    const update = useMutation(api.documents.update);
+
+    const onChange = (content: string) => {
+        update({
+            id: params.documentId,
+            content
+        });
+    };
+
+    if (document === undefined) {
+        return (
+            <div>
+                <Cover.Skeleton />
+                <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+                    <div className="space-y-4 pl-8 pt-4">
+                        <Skeleton className="h-14 w-[50%]" />
+                        <Skeleton className="h-4 w-[80%]" />
+                        <Skeleton className="h-4 w-[40%]" />
+                        <Skeleton className="h-4 w-[60%]" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    if (document === null) {
+        return <div>Not found</div>
+    }
+
+    return (
+        <div className="pb-40">
+            <Cover url={document.coverImage} />
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document} />
+                <Editor 
+                onChange={() => {}}
+                initialContent={document.content}
+                />
+            </div>
+        </div>
+    );
 }
 
-
-
-if(document === null){
-  return <div>Not Found</div>
-}
-  return (
-    <div className="pb-40">
-      {/* <div  className="h-[35vh]"></div> */}
-      <Cover url={document.coverImage}/>
-          <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-<Toolbar initialData={document}/>
-    </div>
-    </div>
-
-  )
-}
-export default DocumentIdPage
+export default DocumentIdPage;
--- components/editor.tsx
+++ components/editor.tsx
@@ -1,60 +1,49 @@
-// "use client";
+"use client";
 
-// import { useTheme } from "next-themes";
-// import {
-//     BlockNoteEditor,
-//     PartialBlock
-// } from "@blocknote/core";
-// import {
-//     BlockNoteView,
-//     useBlockNote
-// } from "@blocknote/react";
-// import "@blocknote/core/style.css";
+import { useTheme } from "next-themes";
+import {
+    BlockNoteEditor,
+    PartialBlock
+} from "@blocknote/core";
+import {
+    BlockNoteView,
+    useBlockNote
+} from "@blocknote/react";
+import "@blocknote/core/style.css";
 
-// import { useEdgeStore } from "@/lib/edgestore";
+import { useEdgeStore } from "@/lib/edgestore";
 
-// interface EditorProps {
-//     onChange: (value: string) => void;
-//     initialContent?: string;
-//     editable?: boolean;
-// };
 
-// const Editor = ({
-//     onChange,
-//     initialContent,
-//     editable
-// }: EditorProps) => {
-//     const { resolvedTheme } = useTheme();
-//     const { edgestore } = useEdgeStore();
 
-//     const handleUpload = async (file: File) => {
-//         const response = await edgestore.publicFiles.upload({
-//             file
-//         });
+interface EditorProps {
+    onChange: (value: string) => void;
+    initialContent?: string;
+    editable?: boolean;
+};
 
-//         return response.url;
-//     }
 
-//     const editor: BlockNoteEditor = useBlockNote({
-//         editable,
-//         initialContent:
-//             initialContent
-//                 ? JSON.parse(initialContent) as PartialBlock[]
-//                 : undefined,
-//         onEditorContentChange: (editor) => {
-//             onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
-//         },
-//         uploadFile: handleUpload
-//     })
+export const Editor = ({onChange,editable,initialContent}:EditorProps) => {
 
-//     return (
-//         <div>
-//             <BlockNoteView
-//                 editor={editor}
-//                 theme={resolvedTheme === "dark" ? "dark" : "light"}
-//             />
-//         </div>
-//     )
-// }
+    const {resolvedTheme} = useTheme()
+    const editor: BlockNoteEditor = useBlockNote({
+        editable,
+        initialContent
+        : initialContent ? JSON.parse(initialContent) as PartialBlock[] : undefined,
+        onEditorContentChange: (editor) => {
+            onChange(JSON.stringify(editor.topLevelBlocks,null,2))
+        },
+    })
+    
+    
+    return (
 
-// export default Editor;
+        <div>
+            <h1>Editor</h1>
+            <BlockNoteView
+            editor={editor}
+            theme={resolvedTheme === "dark" ? "dark": "light"}
+            />
+
+        </div>
+    )
+}
