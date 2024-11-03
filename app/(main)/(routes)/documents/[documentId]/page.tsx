8ff63342262e4cbd6ac0b60a80c15b17dd3028e6
Vanshika <vanshikas2018@gmail.com>
2024-11-03 09:32:13 +0530

    debugging to make the editor page work

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -9,8 +9,9 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Editor } from "@/components/editor";
 
+
+ 
 interface DocumentIdPageProps {
     params: {
         documentId: Id<"documents">;
@@ -59,10 +60,10 @@ const DocumentIdPage = ({
             <Cover url={document.coverImage} />
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
-                <Editor 
-                onChange={() => {}}
-                initialContent={document.content}
-                />
+                <div>
+                  This is where the editor should be but for some reasons its not appearing whatever i do 
+                  </div>
+
             </div>
         </div>
     );
